3b8561da1055f4fed5b3635ec34f163b1f43b023
Noel Vock <noel1uk@gmail.com>
2018-12-12 22:16:26 +0000

    refactor: explicitly specify the base package

--- src/main/java/CDPlayerConfig.java
+++ src/main/java/CDPlayerConfig.java
@@ -2,6 +2,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("soundsystem")
+@ComponentScan(basePackages = "soundsystem")
 public class CDPlayerConfig {
 }
