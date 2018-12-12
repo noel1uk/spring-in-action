c9eeee813920687cce668a98b49a7e9ec291d61b
Noel Vock <noel1uk@gmail.com>
2018-12-12 22:12:38 +0000

    refactor: how to specify a specific package

--- src/main/java/CDPlayerConfig.java
+++ src/main/java/CDPlayerConfig.java
@@ -1,9 +1,7 @@
-package soundsystem;
-
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
+@ComponentScan("soundsystem")
 public class CDPlayerConfig {
 }
--- src/test/java/CDPlayerTest.java
+++ src/test/java/CDPlayerTest.java
@@ -1,15 +1,16 @@
-package soundsystem;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import soundsystem.CompactDisc;
 
 import static org.junit.Assert.assertNotNull;
 
+// Make a Spring application context
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes=CDPlayerConfig.class)
+// Tell the application context to load its configuration from the CDPlayerConfig class
+@ContextConfiguration(classes= CDPlayerConfig.class)
 public class CDPlayerTest {
 
     @Autowired
