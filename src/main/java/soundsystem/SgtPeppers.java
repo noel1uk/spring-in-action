31b447a9117633dcb16c363d5d378c287b961dec
Noel Vock <noel1uk@gmail.com>
2018-12-12 21:02:42 +0000

    initial commit

---
+++ src/main/java/soundsystem/SgtPeppers.java
@@ -0,0 +1,14 @@
+package soundsystem;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SgtPeppers implements CompactDisc {
+
+    private String title = "sgt. Peppers lonely Hearts club band";
+    private String artist = "The Beatles";
+
+    public void play() {
+        System.out.println("Playing " + title + " by " + artist);
+    }
+}
