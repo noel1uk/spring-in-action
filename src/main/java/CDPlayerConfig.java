8de22373336dd2730006d01e645e1ccba06119b9
Noel Vock <noel1uk@gmail.com>
2018-12-13 06:23:30 +0000

    configuration: ComponentScan for classes or interfaces

--- src/main/java/CDPlayerConfig.java
+++ src/main/java/CDPlayerConfig.java
@@ -1,7 +1,8 @@
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import soundsystem.CompactDisc;
 
 @Configuration
-@ComponentScan(basePackages = {"soundsystem", "video"})
+@ComponentScan(basePackageClasses ={CompactDisc.class, DVDPlayer.class})
 public class CDPlayerConfig {
 }
