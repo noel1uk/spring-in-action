0d05e44a6cf80045a42f4b636a950c2fc2e8bd1b
Noel Vock <noel1uk@gmail.com>
2018-12-31 14:55:53 +0000

    remove @ComponentScan and declare Beans manually in config class

--- src/main/java/soundsystem/CDPlayerConfig.java
+++ src/main/java/soundsystem/CDPlayerConfig.java
@@ -1,5 +1,6 @@
 package soundsystem;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import soundsystem.CompactDisc;
@@ -9,11 +10,22 @@ import soundsystem.CompactDisc;
 // process the class and generate Beans to be used in the application
 // It indicates that a class declares one or more @Bean methods and may be processed by
 // the Spring container to generate bean definitions and services at runtime
-@ComponentScan(basePackageClasses ={CompactDisc.class})
+//@ComponentScan(basePackageClasses ={CompactDisc.class})
 // @ComponentScan configures component scanning directives for use with @Configuration
 // classes.
 // Either basePackageClasses() or basePackages() or its alias value() may be specified
 // to define specific packages to scan. If specific packages are not defined scanning will
 // occur from the package of the class that declares this annotation
 public class CDPlayerConfig {
+
+    @Bean
+    public CompactDisc sgtPeppers() {
+        return new SgtPeppers();
+    }
+
+    @Bean
+    public CDPlayer cdPlayer() {
+        return new CDPlayer(sgtPeppers());
+    }
 }
+
